7ed826adaf8de7aa73ffa76d30314885f783c707
seonnn <ssni0105@gmail.com>
2023-03-07 00:30:54 +0900

    feat: layout 내 header 컴포넌트 적용 및 font-family 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { Nanum_Gothic } from 'next/font/google';
+import Header from '@/components/common/Header';
+
+const nanumGothic = Nanum_Gothic({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  preload: false,
+});
 
 export const metadata = {
   title: {
     default: 'PERFRAG',
+    template: 'PERFRAG | %s',
   },
   description: '노트로 향수를 검색할 수 있는 서비스입니다.',
+  icons: {
+    shortcut: '/favicon.ico',
+  },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head />
-      <body>{children}</body>
+      <body className={nanumGothic.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
