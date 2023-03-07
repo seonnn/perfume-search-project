bb893f24cf4857646ccdb4e907c6c48bd4c49a41
seonnn <ssni0105@gmail.com>
2023-03-08 00:35:26 +0900

    chore: tailwind config theme 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-noto-sans-kr)'],
+      },
+      width: {
+        69: '17.25rem',
+      },
+      padding: {
+        22.5: '5.625rem',
+      },
+    },
   },
   plugins: [],
 };
