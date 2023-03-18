9c4b02e00dd4a107fe86142109154b543e74bc08
seonnn <ssni0105@gmail.com>
2023-03-18 23:58:04 +0900

    style: props 변수명 변경

--- components/common/FilterItem.tsx
+++ components/common/FilterItem.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FaCheckSquare, FaRegCheckSquare } from 'react-icons/fa';
 
-interface FilterItem {
+interface FilterItemProps {
   item: string;
 }
 
-function FilterItem({ item }: FilterItem) {
+function FilterItem({ item }: FilterItemProps) {
   return (
     <li className="flex items-center gap-2 pl-2 py-1 text-stone-600" key={item}>
       {/* selected */}
       {/* <FaCheckSquare className="text-stone-600" size={16} /> */}
-      <FaRegCheckSquare className="text-stone-400 cursor-pointer" size={16} />
-      <span className="cursor-pointer">{item}</span>
+      <FaRegCheckSquare className="text-stone-400" size={16} />
+      <input type="checkbox" value={item} className="absolute appearance-none w-4 h-4 cursor-pointer" />
+      <label className="cursor-pointer">{item}</label>
     </li>
   );
 }
