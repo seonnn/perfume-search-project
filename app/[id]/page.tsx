d2bc489e915b35f508cbc3736cfcd04c4f5c3baf
seonnn <ssni0105@gmail.com>
2023-03-23 23:48:20 +0900

    feat: PerfumeDetail 페이지 레이아웃 및 dummy data 적용

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import NoteList from '@/components/detail/NoteList';
 import { perfumeList } from '@/utils/noteList';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
@@ -10,8 +11,21 @@ function PerfumeDetail() {
 
   if (!item) return <div>Loading...</div>;
   return (
-    <div className="my-44 border-2">
-      <Image src={item.imgUrl} alt={item.name} width={500} height={500} property="true" />
+    <div className="h-screen w-full flex justify-center items-center border-2 border-red-500">
+      <div className="box-border w-full max-w-7xl flex justify-start gap-24 border-4 border-blue-500">
+        <Image src={item.imgUrl} alt={item.name} width={500} height={500} property="true" className="border-2" />
+        <div>
+          <div className="flex flex-col gap-4">
+            <h3 className="text-stone-600 text-xl">{item.brand}</h3>
+            <h2 className="text-stone-800 font-bold text-3xl">{item.name}</h2>
+          </div>
+          <div className="flex flex-col w-full mt-6 gap-4">
+            <NoteList noteList={item.topNote} position="top" />
+            <NoteList noteList={item.heartNote} position="middle" />
+            <NoteList noteList={item.baseNote} position="base" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
