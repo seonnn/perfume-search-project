edff750026a7b4c1ef8a78e17cc0d734df0b693e
seonnn <ssni0105@gmail.com>
2023-04-23 23:31:19 +0900

    refactor: data type 변경에 따른 코드 리팩토링

--- components/common/SideFilterMenu.tsx
+++ components/common/SideFilterMenu.tsx
@@ -1,4 +1,4 @@
-import { noteList } from '@/utils/noteList';
+import { fragranceList, noteList } from '@/utils/noteList';
 import React from 'react';
 import DropDown from '../home/DropDown';
 import SearchBar from '../home/SearchBar';
@@ -10,14 +10,33 @@ function SideFilterMenu() {
     <aside className="w-69 mt-22.5">
       <div className="border-y-1 py-3 px-4">
         <h3 className="text-xl text-stone-800 font-bold py-2">Note Filter</h3>
-        {Object.keys(noteList).map((fragrance) => (
-          <DropDown fragrance={fragrance} key={fragrance} />
+        {fragranceList.map((fragrance) => (
+          <DropDown
+            title={fragrance.f_name}
+            key={fragrance.f_id}
+            dropDownList={noteList
+              .filter((note) => note.f_id === fragrance.f_id)
+              .map((note) => {
+                return {
+                  id: note.n_id,
+                  name: note.n_name,
+                };
+              })}
+          />
         ))}
       </div>
       <div className="border-b-1 py-3 px-4">
         <h3 className="text-xl text-stone-800 font-bold py-2">Brands</h3>
         <SearchBar />
-        <FilterList list={brandList} type={'brand'} />
+        <FilterList
+          list={brandList.map((brand) => {
+            return {
+              id: brand.b_id,
+              name: brand.b_name,
+            };
+          })}
+          type={'brand'}
+        />
       </div>
     </aside>
   );
