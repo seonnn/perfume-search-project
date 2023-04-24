310f50bf32c79e21ad31abfef9b7c9faf34aff0d
seonnn <ssni0105@gmail.com>
2023-04-24 22:41:55 +0900

    feat: useCustomSearchParams hook 추가

---
+++ hooks/useCustomSearchParams.ts
@@ -0,0 +1,26 @@
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+function useCustomSearchParams<T extends Partial<{ [key: string]: number[] }>>() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const urlSearchParams = searchParams ? new URLSearchParams(searchParams) : null;
+
+  function setSearchParams(params: Partial<T>) {
+    if (!urlSearchParams) return console.error('query parameter 없음!');
+
+    Object.entries(params).forEach(([key, value]) => {
+      if (!value.length) urlSearchParams?.delete(key);
+      else urlSearchParams?.set(key, String(value.join('|')));
+    });
+
+    const paramsToString = urlSearchParams?.toString();
+    const query = paramsToString ? `?${paramsToString}` : '';
+
+    router.push(`${pathname}${query}`);
+  }
+
+  return { searchParams, setSearchParams };
+}
+
+export default useCustomSearchParams;
