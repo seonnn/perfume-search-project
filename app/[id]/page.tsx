e3178821d49d9d3b56341ad1a24fc00bb4b79fdd
seonnn <ssni0105@gmail.com>
2023-04-27 23:47:33 +0900

    refactor: data type 변경에 따른 코드 리팩토링

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { perfumeList } from '@/utils/noteList';
+import { brandList, perfumeList } from '@/utils/noteList';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import React from 'react';
@@ -16,7 +16,7 @@ function PerfumeDetail() {
         <Image src={item.imgUrl} alt={item.name} width={500} height={500} className="bg-stone-50" />
         <div className="h-125">
           <div className="flex flex-col gap-4">
-            <h3 className="text-stone-600 text-xl">{item.brand}</h3>
+            <h3 className="text-stone-600 text-xl">{brandList.find((brand) => brand.b_id === item.b_id)?.b_name}</h3>
             <h2 className="text-stone-800 font-bold text-3xl">{item.name}</h2>
           </div>
           <div className="flex flex-col w-full mt-6 gap-4">
