0edc3fa8328fd0d8f2424ca8c80d7d75ee2e53e4
seonnn <ssni0105@gmail.com>
2023-05-08 00:01:34 +0900

    feat: SideFilterMenu props 추가

--- components/common/SideFilterMenu.tsx
+++ components/common/SideFilterMenu.tsx
@@ -1,28 +1,22 @@
-import { fragranceList, noteList } from '@/utils/noteList';
+import { fragranceList } from '@/utils/noteList';
 import React from 'react';
 import DropDown from '../home/DropDown';
 import SearchBar from '../home/SearchBar';
 import { brandList } from '@/utils/noteList';
 import FilterList from './FilterList';
+import { NoteList } from '@/types';
 
-function SideFilterMenu() {
+interface SideFilterMenuProps {
+  noteList: NoteList[];
+}
+
+function SideFilterMenu({ noteList }: SideFilterMenuProps) {
   return (
     <aside className="w-69 mt-22.5">
       <div className="border-y-1 py-3 px-4">
         <h3 className="text-xl text-stone-800 font-bold py-2">Note Filter</h3>
         {fragranceList.map((fragrance) => (
-          <DropDown
-            title={fragrance.f_name}
-            key={fragrance.f_id}
-            dropDownList={noteList
-              .filter((note) => note.f_id === fragrance.f_id)
-              .map((note) => {
-                return {
-                  id: note.n_id,
-                  name: note.n_name,
-                };
-              })}
-          />
+          <DropDown title={fragrance.name} key={fragrance.id} dropDownList={noteList[fragrance.id - 1].noteList} />
         ))}
       </div>
       <div className="border-b-1 py-3 px-4">
