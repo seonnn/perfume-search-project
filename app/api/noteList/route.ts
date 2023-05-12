a14c921235aaf5d3c3b0c895ce73a59205fec4af
seonnn <ssni0105@gmail.com>
2023-05-12 22:49:13 +0900

    refactor: getNoteList 함수 리팩토링

---
+++ app/api/noteList/route.ts
@@ -0,0 +1,37 @@
+import { NoteListResponseData } from '@/types/response';
+import { supabase } from '@/utils/supabase/supabase';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    const { data } = await supabase
+      .from('fragrance_list')
+      .select(
+        `
+      f_id,
+      note_list (
+        n_id,
+        n_name
+      )
+    `
+      )
+      .returns<NoteListResponseData[]>();
+
+    if (!data || !data.length) {
+      return NextResponse.json([]);
+    }
+
+    return NextResponse.json(
+      data.map((note) => {
+        const noteList = note.note_list.map((item) => {
+          return { id: item.n_id, name: item.n_name };
+        });
+
+        return { fragranceId: note.f_id, noteList };
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    throw new Error('노트 조회 실패');
+  }
+}
