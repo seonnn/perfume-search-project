0219331c7d15c485faff720c6f1ddd80c49b416a
seonnn <ssni0105@gmail.com>
2023-05-16 00:35:14 +0900

    design: 반응형 디자인 적용을 위한 스타일 변경

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '@/public/logo.svg';
+// import { FaBars } from 'react-icons/fa';
 
 function Header() {
   return (
     <nav className="fixed top-0 left-0 flex justify-center w-full h-24 bg-stone-200 z-10">
-      <div className="flex w-full max-w-screen-xl justify-between items-center text-stone-400">
+      <div className="flex w-full max-w-screen-xl justify-between items-center mx-5 text-stone-400">
         <Link href="/">
           <Image src={Logo} alt="logo" width={208} height={34} />
         </Link>
-        {/* <div className="flex gap-6 font-bold text-lg">
+        {/* <div className="flex gap-8 font-bold text-lg max-md:hidden">
           <div>향수 검색</div>
-          <div>컬러 추천</div>
+          <div>계절&색 추천</div>
+        </div>
+        <div className="md:hidden">
+          <FaBars size={24} />
         </div> */}
       </div>
     </nav>
