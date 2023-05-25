c08a4122e9b35b249e0c5459ab40cf3435290f7b
seonnn <ssni0105@gmail.com>
2023-05-26 03:07:56 +0900

    feat: 향수 이미지 sizes 속성 추가

--- components/home/PerfumeCard.tsx
+++ components/home/PerfumeCard.tsx
@@ -12,7 +12,13 @@ function PerfumeCard({ brand, name, imgUrl, id }: PerfumeCardProps) {
   return (
     <div className="flex flex-col gap-3 cursor-pointer">
       <div className="flex justify-center items-center relative bg-stone-50 pt-[100%]">
-        <Image src={imgUrl} alt={name} fill className="object-cover" />
+        <Image
+          src={imgUrl}
+          alt={name}
+          fill
+          className="object-cover"
+          sizes="(min-width: 1280px) 25vw, (min-width: 1024px) 33vw, 50vw"
+        />
       </div>
       <div className="text-sm text-stone-500">{brand}</div>
       <div className="text-stone-800">{name}</div>
