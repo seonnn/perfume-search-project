9b40770652c9d0088137f5c4cdd44b7809badcb4
seonnn <ssni0105@gmail.com>
2023-06-14 13:12:53 +0900

    fix: naver 로그인 버튼 배경색 적용되지 않는 오류 해결

--- components/login/SocialLoginButton.tsx
+++ components/login/SocialLoginButton.tsx
@@ -6,16 +6,16 @@ import NaverLogo from '@/public/socialLogos/naver.svg';
 const option = {
   google: {
     name: '구글',
-    bg: 'white',
-    border: 'stone-400',
-    text: 'stone-800',
+    bg: 'bg-white',
+    border: 'border-stone-300',
+    text: 'bg-stone-800',
     logo: GoogleLogo,
   },
   naver: {
     name: '네이버',
-    bg: 'green-naver',
-    border: 'green-naver',
-    text: 'white',
+    bg: 'bg-green-naver',
+    border: 'border-green-naver',
+    text: 'text-white',
     logo: NaverLogo,
   },
 };
@@ -24,9 +24,7 @@ function SocialLoginButton({ type }: { type: 'google' | 'naver' }) {
   const { name, bg, border, text, logo } = option[type];
 
   return (
-    <button
-      className={`flex justify-center items-center gap-3 border-1 rounded py-3 w-full bg-${bg} border-${border} text-${text}`}
-    >
+    <button className={`flex justify-center items-center gap-3 rounded py-3 w-full border-1 ${bg} ${border} ${text}`}>
       <Image src={logo} alt={name} width={24} height={24} />
       {name}로 시작하기
     </button>
