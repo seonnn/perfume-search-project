363a7dc4181b0379c416f460822df17fd69f83d2
seonnn <ssni0105@gmail.com>
2023-06-22 23:53:32 +0900

    feat: 이미지 파일 업로드 기능 추가

--- app/admin/perfume/add/page.tsx
+++ app/admin/perfume/add/page.tsx
@@ -2,7 +2,8 @@
 import NoteFilterModal from '@/components/admin/NoteFilterModal';
 import NoteInput from '@/components/admin/NoteInput';
 import Loading from '@/components/common/Loading';
-import { Brand, Note } from '@/types';
+import { Brand } from '@/types';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { BsPlus } from 'react-icons/bs';
 // BsPlus
@@ -15,7 +16,7 @@ export interface SelectedNoteList {
 }
 
 function Page() {
-  const [image, setImage] = useState();
+  const [imageState, setImageState] = useState<{ imageFile: File; imageUrl: string }>();
   const [brand, setBrand] = useState('');
   const [brandList, setBrandList] = useState<Brand[]>();
   const [noteList, setNoteList] = useState();
@@ -34,6 +35,37 @@ function Page() {
       : [...prevNoteList, id];
 
     setSelectedNoteList({ ...selectedNoteList, [type]: newNoteList });
+  };
+
+  const setImagePreview = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+
+    const imageFile = event.target.files[0];
+
+    if (!imageFile.type.includes('image/')) return window.alert('이미지 파일만 업로드 가능합니다.');
+    if (imageFile.size > 512000) return window.alert('500kb 미만의 파일만 업로드 가능합니다.');
+
+    const imageUrl = URL.createObjectURL(imageFile);
+
+    setImageState({ imageFile: imageFile, imageUrl: imageUrl });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!imageState) return window.alert('향수 이미지를 입력해주세요!');
+
+    const formData = new FormData();
+
+    formData.append('image', imageState.imageFile);
+    formData.append('imagename', JSON.stringify({ imagename: imageState.imageFile.name }));
+
+    const response = await fetch('/api/perfume', {
+      method: 'POST',
+      body: formData,
+    }).then((res) => res.json());
+
+    console.log('response:' + JSON.parse(response));
   };
 
   useEffect(() => {
@@ -66,17 +98,21 @@ function Page() {
         />
       )}
       <h2 className="text-2xl font-bold mb-16">향수 등록</h2>
-      <form className="w-full flex flex-col items-center gap-4">
-        <div className="w-48 h-48 flex justify-center items-center bg-stone-100 text-stone-600 mb-20">
+      <form className="w-full flex flex-col items-center gap-4" onSubmit={handleSubmit}>
+        <div className="w-48 h-48 flex justify-center items-center bg-stone-100 text-stone-600 mb-20 cursor-pointer">
           <label htmlFor="image">
-            <BsPlus size={48} />
+            {imageState ? (
+              <Image src={imageState.imageUrl} alt="perfumeImage" width={192} height={192} />
+            ) : (
+              <BsPlus size={48} />
+            )}
           </label>
-          <input className="hidden" type="file" accept="image/*" id="image" />
+          <input className="hidden" onChange={setImagePreview} type="file" accept="image/*" id="image" name="imgae" />
         </div>
         <div className="w-full grid grid-cols-2 gap-8">
           <div className="flex items-center">
             <label className="flex w-24 shrink-0">향수명:</label>
-            <input className="flex grow border-1 border-stone-300 p-3" />
+            <input className="flex grow border-1 border-stone-300 p-3" name="p_name" />
           </div>
           <div className="flex items-center">
             <label className="flex w-24 shrink-0">브랜드명:</label>
@@ -86,6 +122,7 @@ function Page() {
               }`}
               value={brand}
               onChange={handleBrandChange}
+              name="b_name"
             >
               <option value={0} className="text-stone-600" hidden>
                 브랜드를 선택해주세요.
