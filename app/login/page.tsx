2e6cc1e8ad2b005ec265fbdb2ab7162ccac34319
seonnn <ssni0105@gmail.com>
2023-06-30 05:57:24 +0900

    feat: 로그인 api 연결

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Logo from '@/public/logo.svg';
 import Image from 'next/image';
-import SocialLoginButton from '@/components/login/SocialLoginButton';
+import LabelInput from '@/components/common/LabelInput';
+import Button from '@/components/common/Button';
+// import SocialLoginButton from '@/components/login/SocialLoginButton';
 
 function Page() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+      }).then((res) => res.json());
+
+      console.log(response.data.session);
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="flex flex-col items-center border-1 border-stone-300 rounded w-96 px-6 py-14 gap-8">
+      <div className="flex flex-col items-center border-1 border-stone-300 rounded w-[458px] px-7 py-10 gap-8">
         <Image src={Logo} alt="logo" width={208} height={34} />
-        <div className="flex flex-col gap-4 w-full">
+        <form className="w-full flex flex-col gap-4 mt-2" onSubmit={handleSubmit}>
+          <div className="flex items-center">
+            <LabelInput
+              label="아이디"
+              state={email}
+              setState={setEmail}
+              placeholder="이메일을 입력해주세요."
+              type="email"
+            />
+          </div>
+          <div className="flex items-center mb-4">
+            <LabelInput
+              label="비밀번호"
+              state={password}
+              setState={setPassword}
+              placeholder="비밀번호를 입력해주세요."
+              type="password"
+            />
+          </div>
+          <Button text="로그인" />
+          {/* <div className="flex flex-col gap-4 w-full">
           <SocialLoginButton type="google" />
           <SocialLoginButton type="naver" />
-        </div>
+        </div> */}
+        </form>
       </div>
     </div>
   );
