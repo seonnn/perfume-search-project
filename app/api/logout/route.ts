3154125b7c578c743a1c983ac8174c5bc0589ae8
seonnn <ssni0105@gmail.com>
2023-07-01 19:21:43 +0900

    feat: 로그아웃 api 추가

---
+++ app/api/logout/route.ts
@@ -0,0 +1,17 @@
+import { supabase } from '@/utils/supabase/supabase';
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error(error);
+    throw new Error('로그아웃 실패');
+  }
+
+  const response = NextResponse.json({ status: 204 });
+
+  response.cookies.delete('access_token');
+
+  return response;
+}
