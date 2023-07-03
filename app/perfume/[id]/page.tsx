2e467ad1c6990187d2c94233cfc7ea47e2be0b27
seonnn <ssni0105@gmail.com>
2023-07-03 10:05:36 +0900

    refactor: fetchCache 옵션 삭제

--- app/perfume/[id]/page.tsx
+++ app/perfume/[id]/page.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import NoteBadges from '@/components/detail/NoteBadges';
 import { getPerfumeDetail } from '@/utils/supabase/getPerfumeDetail';
 
-// export const dynamic = 'force-dynamic';
-export const fetchCache = 'force-cache';
+export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 
 async function Page({ params }: { params: { id: string } }) {
