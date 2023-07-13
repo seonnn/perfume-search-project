9869dc6fd3b329ba56b0a5f5a4bd449811308e5a
seonnn <ssni0105@gmail.com>
2023-07-13 17:21:58 +0900

    refactor: withAuth hook 사용자 권한 확인 방식 리팩토링

--- components/common/hocs/withAuth.tsx
+++ components/common/hocs/withAuth.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useRecoilValue } from 'recoil';
+import { userAtom } from '@/recoil/atom';
+import jwt_decode from 'jwt-decode';
 
 export function withAuth<P>(Component: React.ComponentType<P & { children: React.ReactNode }>) {
   const WrappedComponent = ({ ...props }: P & { children: React.ReactNode }) => {
     const router = useRouter();
-    const [isAuthUser, setIsAuthUser] = useState(false);
+    const user = useRecoilValue(userAtom);
+    const { email }: { email: string } = jwt_decode(user);
 
     useEffect(() => {
-      if (localStorage.getItem('user')) {
-        setIsAuthUser(true);
-      } else {
+      if (email !== 'surfragmanager@gmail.com' && email !== 'admin@surfrag.test') {
         window.alert('본 페이지는 관리자 회원만 접근 가능합니다. 이용 권한이 없어 메인 페이지로 이동합니다.');
         router.push('/');
       }
     }, []);
 
-    return isAuthUser ? <Component {...props} /> : null;
+    return email !== 'surfragmanager@gmail.com' && email !== 'admin@surfrag.test' ? null : <Component {...props} />;
   };
 
   WrappedComponent.displayName = `withAuth(${Component.displayName || Component.name})`;
