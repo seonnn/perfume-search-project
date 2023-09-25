e65b51d7cc4e2f507180677310a390668190ae08
seonnn <ssni0105@gmail.com>
2023-09-25 23:31:18 +0900

    feat: userAtom recoil-persist 설정 추가

--- recoil/atom.ts
+++ recoil/atom.ts
@@ -1,11 +1,15 @@
 import { atom, atomFamily } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 export const modalAtomFamily = atomFamily({
   key: 'modalAtomFamily',
   default: false,
 });
 
+const { persistAtom } = recoilPersist({ key: 'userAtom', storage: localStorage });
+
 export const userAtom = atom({
   key: 'userAtom',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
