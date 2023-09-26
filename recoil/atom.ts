31a9bcd6598935abe177551a0e91ba17be6eed3c
seonnn <ssni0105@gmail.com>
2023-09-26 23:43:49 +0900

    refactor: userAtom recoil-persist 설정 제거

--- recoil/atom.ts
+++ recoil/atom.ts
@@ -1,15 +1,11 @@
 import { atom, atomFamily } from 'recoil';
-import { recoilPersist } from 'recoil-persist';
 
 export const modalAtomFamily = atomFamily({
   key: 'modalAtomFamily',
   default: false,
 });
 
-const { persistAtom } = recoilPersist({ key: 'userAtom', storage: localStorage });
-
 export const userAtom = atom({
   key: 'userAtom',
   default: '',
-  effects_UNSTABLE: [persistAtom],
 });
