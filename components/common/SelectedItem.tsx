8e870182bc783f2a813e27c234483fc970873186
seonnn <ssni0105@gmail.com>
2023-10-08 23:57:21 +0900

    feat: SelectedItem 컴포넌트 UI 추가

--- components/common/SelectedItem.tsx
+++ components/common/SelectedItem.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { IoClose } from 'react-icons/io5';
 
-function SelectedItem() {
-  return <div>SelectedItem</div>;
+interface SelectedItemProps {
+  text: string;
+  handleSelected: () => void;
+}
+
+function SelectedItem({ text, handleSelected }: SelectedItemProps) {
+  return (
+    <span className="flex gap-1 items-center font-bold p-2 text-sm bg-beige-400 text-white rounded ">
+      {text}
+      <IoClose size={16} onClick={handleSelected} />
+    </span>
+  );
 }
 
 export default SelectedItem;
