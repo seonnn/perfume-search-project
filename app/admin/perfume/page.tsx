d87d09565cab4daa7cf7318f215b4d9fd9f68075
seonnn <ssni0105@gmail.com>
2024-02-26 11:40:32 +0900

    refactor: PerfumeListTable 컴포넌트 분리

--- app/admin/perfume/page.tsx
+++ app/admin/perfume/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Image from 'next/image';
-import { adminTableHeader } from '@/utils/admin';
 import Loading from '@/components/common/Loading';
 import Link from 'next/link';
 import Button from '@/components/common/Button';
 import { getPerfumeList } from '@/utils/supabase/getPerfumeList';
+import PerfumeListTable from '@/components/admin/PerfumeListTable';
 
 export const dynamic = 'force-dynamic';
 
@@ -20,36 +19,7 @@ async function Page() {
           <Button text="향수 등록" />
         </Link>
       </div>
-      <table className="w-full text-stone-800">
-        <thead>
-          <tr className="border-y-1">
-            {adminTableHeader['perfume'].map((headerName) => (
-              <th className="font-normal py-2 px-2" key={headerName}>
-                {headerName}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {perfumeList.map((perfume) => (
-            <tr key={perfume.id} className="border-b-1">
-              <td className="text-center">{perfume.id}</td>
-              <td className="flex justify-center py-2">
-                <Link href={`/admin/perfume/edit/${perfume.id}`}>
-                  <Image
-                    src={process.env.NEXT_PUBLIC_SUPABASE_URL + perfume.imgUrl}
-                    alt="향수 이미지"
-                    width={128}
-                    height={128}
-                  />
-                </Link>
-              </td>
-              <td className="text-center">{perfume.brand}</td>
-              <td className="text-center">{perfume.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <PerfumeListTable />
     </main>
   );
 }
