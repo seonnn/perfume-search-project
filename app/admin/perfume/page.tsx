704c10dfd9f75cba546e8af7b78721d748459864
seonnn <ssni0105@gmail.com>
2024-02-26 11:44:51 +0900

    refactor: 불필요한 코드 삭제

--- app/admin/perfume/page.tsx
+++ app/admin/perfume/page.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Loading from '@/components/common/Loading';
 import Link from 'next/link';
 import Button from '@/components/common/Button';
-import { getPerfumeList } from '@/utils/supabase/getPerfumeList';
 import PerfumeListTable from '@/components/admin/PerfumeListTable';
 
-export const dynamic = 'force-dynamic';
-
-async function Page() {
-  const perfumeList = await getPerfumeList();
-
-  if (!perfumeList) return <Loading />;
+function Page() {
   return (
     <main className="w-full flex flex-col justify-start items-center gap-5">
       <h2 className="text-2xl text-stone-800 font-bold">향수 목록 관리</h2>
