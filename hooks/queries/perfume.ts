2ebc782b24e75c3423d8bb8739e443172645ef62
seonnn <ssni0105@gmail.com>
2024-02-26 11:41:48 +0900

    feat: useGetPerfumeList, usePostPerfume hook 추가

---
+++ hooks/queries/perfume.ts
@@ -0,0 +1,23 @@
+import { getPerfumeList } from '@/utils/supabase/getPerfumeList';
+import { postPerfume } from '@/utils/supabase/postPerfume';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+export const useGetPerfumeList = () => {
+  return useQuery({ queryKey: ['perfumeList'], queryFn: getPerfumeList, staleTime: Infinity });
+};
+
+export const usePostPerfume = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: postPerfume,
+    onSuccess: (data) => {
+      if (data.status === 409) {
+        window.alert('이미 등록된 향수입니다.');
+      } else {
+        window.alert('향수 등록이 완료되었습니다.');
+        queryClient.invalidateQueries({ queryKey: ['perfumeList'] });
+      }
+    },
+  });
+};
