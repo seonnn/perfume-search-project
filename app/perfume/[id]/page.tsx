fa8584ca863b951829c8f3d551d65b7f5d7e917e
seonnn <ssni0105@gmail.com>
2024-02-27 12:00:25 +0900

    test: 향수 상세 페이지 리팩토링 이전 버전 배포 테스트

--- app/perfume/[id]/page.tsx
+++ app/perfume/[id]/page.tsx
@@ -1,38 +1,40 @@
+'use client';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NoteBadges from '@/components/detail/NoteBadges';
+import { PerfumeDetail } from '@/types';
 import { getPerfumeDetail } from '@/utils/supabase/getPerfumeDetail';
-import { getPerfumeList } from '@/utils/supabase/getPerfumeList';
-import { Perfume } from '@/types';
 
-export const revalidate = 3;
+function PerfumeDetail({ params }: { params: { id: string } }) {
+  const [perfume, setPerfume] = useState<PerfumeDetail | null>();
+  console.log(perfume);
 
-async function Page({ params }: { params: { id: string } }) {
-  const perfume = await getPerfumeDetail(params.id);
+  const fetchPerfumeDetail = async () => {
+    const { name, imgUrl, brandName, perfumeNoteList } = await getPerfumeDetail(params.id);
+    setPerfume({ name, imgUrl, brandName, perfumeNoteList });
+  };
+
+  useEffect(() => {
+    fetchPerfumeDetail();
+  }, []);
 
   if (!perfume) return <div>Loading...</div>;
   return (
-    <div className="h-screen w-full flex justify-center items-center max-xl:h-full max-xl:mx-5 max-xl:my-48 max-sm:my-32">
-      <div className="box-border w-full max-w-screen-xl flex justify-start gap-24 max-xl:w-auto max-xl:flex-col max-xl:items-center max-xl:gap-8">
-        <div className="flex flex-col w-full justify-start gap-4 xl:hidden">
-          <h3 className="text-stone-600 text-xl max-sm:text-lg max-xs:text-base">{perfume.brandName}</h3>
-          <h2 className="text-stone-800 font-bold text-3xl max-sm:text-2xl max-xs:text-xl">{perfume.name}</h2>
-        </div>
-        <div className="relative flex justify-center bg-stone-50 max-xl:w-full">
-          <Image
-            src={process.env.NEXT_PUBLIC_SUPABASE_URL + perfume.imgUrl}
-            alt={perfume.name}
-            width={500}
-            height={500}
-            className="max-xl:w-3/5 max-xl:h-3/5"
-          />
-        </div>
-        <div>
-          <div className="flex flex-col gap-4 max-xl:hidden">
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="box-border w-full max-w-7xl flex justify-start gap-24">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/${perfume.imgUrl}`}
+          alt={perfume.name}
+          width={500}
+          height={500}
+          className="bg-stone-50"
+        />
+        <div className="h-125">
+          <div className="flex flex-col gap-4">
             <h3 className="text-stone-600 text-xl">{perfume.brandName}</h3>
             <h2 className="text-stone-800 font-bold text-3xl">{perfume.name}</h2>
           </div>
-          <div className="flex flex-col w-full mt-6 gap-4 max-xl:mt-0">
+          <div className="flex flex-col w-full mt-6 gap-4">
             <NoteBadges list={perfume.perfumeNoteList.topNote} position="top" />
             <NoteBadges list={perfume.perfumeNoteList.middleNote} position="middle" />
             <NoteBadges list={perfume.perfumeNoteList.baseNote} position="base" />
@@ -43,10 +45,4 @@ async function Page({ params }: { params: { id: string } }) {
   );
 }
 
-export default Page;
-
-export async function generateStaticParams() {
-  const perfumeListData = await getPerfumeList();
-
-  return perfumeListData.map((perfume: Perfume) => ({ id: perfume.id.toString() }));
-}
+export default PerfumeDetail;
