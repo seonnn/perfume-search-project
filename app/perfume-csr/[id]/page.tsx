9cbcf811c31e56b53205e07ff5c0eea7de5ddaf5
seonnn <ssni0105@gmail.com>
2024-02-28 14:28:46 +0900

    test: 상세페이지 렌더링 방식별 속도 테스트

---
+++ app/perfume-csr/[id]/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { PerfumeDetail } from '@/types';
+import { getPerfumeDetail } from '@/utils/supabase/getPerfumeDetail';
+import DetailContents from '@/components/detail/DetailContents';
+
+function PerfumeDetail({ params }: { params: { id: string } }) {
+  const [perfume, setPerfume] = useState<PerfumeDetail | null>();
+  console.log(perfume);
+
+  const fetchPerfumeDetail = async () => {
+    const { name, imgUrl, brandName, perfumeNoteList } = await getPerfumeDetail(params.id);
+    setPerfume({ name, imgUrl, brandName, perfumeNoteList });
+  };
+
+  useEffect(() => {
+    fetchPerfumeDetail();
+  }, []);
+
+  if (!perfume) return <div>Loading...</div>;
+  return (
+    <DetailContents
+      name={perfume.name}
+      imgUrl={perfume.imgUrl}
+      brandName={perfume.brandName}
+      perfumeNoteList={perfume.perfumeNoteList}
+    />
+  );
+}
+
+export default PerfumeDetail;
---
+++ components/detail/DetailContents.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+import NoteBadges from './NoteBadges';
+import { PerfumeNoteList } from '@/types';
+
+interface DetailContentsProps {
+  name: string;
+  imgUrl: string;
+  brandName: string;
+  perfumeNoteList: PerfumeNoteList;
+}
+
+const DetailContents = ({ name, imgUrl, brandName, perfumeNoteList }: DetailContentsProps) => {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="box-border w-full max-w-7xl flex justify-start gap-24">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/${imgUrl}`}
+          alt={name}
+          width={500}
+          height={500}
+          className="bg-stone-50"
+        />
+        <div className="h-125">
+          <div className="flex flex-col gap-4">
+            <h3 className="text-stone-600 text-xl">{brandName}</h3>
+            <h2 className="text-stone-800 font-bold text-3xl">{name}</h2>
+          </div>
+          <div className="flex flex-col w-full mt-6 gap-4">
+            <NoteBadges list={perfumeNoteList.topNote} position="top" />
+            <NoteBadges list={perfumeNoteList.middleNote} position="middle" />
+            <NoteBadges list={perfumeNoteList.baseNote} position="base" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailContents;
