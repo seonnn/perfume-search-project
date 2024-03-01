c2e389da2c49a00a9b35d930e3c41173b0051a70
seonnn <ssni0105@gmail.com>
2024-03-01 23:53:43 +0900

    feat: props 추가(placeholder props 추가)

--- components/home/SearchBar.tsx
+++ components/home/SearchBar.tsx
@@ -3,14 +3,16 @@ import { AiOutlineSearch } from 'react-icons/ai';
 
 interface SearchBarProps {
   setSearchKeyWord: Dispatch<SetStateAction<string>>;
+  placeholder?: string;
 }
 
-function SearchBar({ setSearchKeyWord }: SearchBarProps) {
+function SearchBar({ setSearchKeyWord, placeholder }: SearchBarProps) {
   return (
     <label className="relative">
       <input
         className="flex text-sm text-stone-700 pl-3 pr-7 py-1.5 bg-stone-200 w-full rounded my-2"
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSearchKeyWord(event.target.value)}
+        placeholder={placeholder}
       />
       <div className="absolute top-1.5 right-2 bg-stone-200">
         <AiOutlineSearch size={20} />
