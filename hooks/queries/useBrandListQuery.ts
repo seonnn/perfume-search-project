9e2cf8fbb2630b466cab721d700f677b97cc9996
seonnn <ssni0105@gmail.com>
2024-03-03 05:07:05 +0900

    fix: useGetBrandList query key 수정

--- hooks/queries/useBrandListQuery.ts
+++ hooks/queries/useBrandListQuery.ts
@@ -2,5 +2,5 @@ import { getBrandList } from '@/utils/supabase/getBrandList';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetBrandList = () => {
-  return useQuery({ queryKey: ['noteList'], queryFn: getBrandList, staleTime: 300 });
+  return useQuery({ queryKey: ['brandList'], queryFn: getBrandList, staleTime: 300 });
 };
