0294a95bb25ae4dbaa0071d4a68d92da65ebcea2
seonnn <ssni0105@gmail.com>
2024-03-04 02:39:41 +0900

    feat: useGetFilterAndSearchPerfumeList 함수 추가

--- hooks/queries/usePerfumeListQuery.ts
+++ hooks/queries/usePerfumeListQuery.ts
@@ -1,14 +1,26 @@
-import { getFilteredPerfumeList, getPerfumeList } from '@/utils/supabase/getPerfumeList';
+import { getFilteredPerfumeList, getPerfumeList, getFilterAndSearchPerfumeList } from '@/utils/supabase/getPerfumeList';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetPerfumeList = () => {
-  return useQuery({ queryKey: ['perfumeList'], queryFn: getPerfumeList, staleTime: 300 });
+  return useQuery({
+    queryKey: ['perfumeList'],
+    queryFn: getPerfumeList,
+    staleTime: 300000,
+  });
 };
 
 export const useGetFilteredPerfumeList = (notes: string, brands: string) => {
   return useQuery({
     queryKey: ['filteredPerfumeList', notes, brands],
     queryFn: () => getFilteredPerfumeList(notes, brands),
-    staleTime: 300,
+    staleTime: 300000,
+  });
+};
+
+export const useGetFilterAndSearchPerfumeList = (keyword: string, notes: string, brands: string) => {
+  return useQuery({
+    queryKey: ['filterAndSearchPerfumeList', keyword, notes, brands],
+    queryFn: () => getFilterAndSearchPerfumeList(keyword, notes, brands),
+    staleTime: 300000,
   });
 };
