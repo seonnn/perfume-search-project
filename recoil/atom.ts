cf10176c9e56fc3353116a18df95c46604c1a8b1
seonnn <ssni0105@gmail.com>
2024-03-05 01:35:10 +0900

    feat: searchKeywordAtom 추가

--- recoil/atom.ts
+++ recoil/atom.ts
@@ -1,4 +1,4 @@
-import { atom, atomFamily } from 'recoil';
+import { AtomEffect, atom, atomFamily } from 'recoil';
 
 export const modalAtomFamily = atomFamily({
   key: 'modalAtomFamily',
@@ -8,4 +8,26 @@ export const modalAtomFamily = atomFamily({
 export const userAtom = atom({
   key: 'userAtom',
   default: '',
+});
+
+const localStorage = typeof window !== 'undefined' ? window.localStorage : null;
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key: string) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage && localStorage.getItem(key);
+    if (savedValue !== null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      if (isReset) return localStorage && localStorage.removeItem(key);
+
+      return localStorage && localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+export const searchKeywordAtom = atom<string[]>({
+  key: 'searchKeywordAtom',
+  default: [],
+  effects: [localStorageEffect<string[]>('keyword')],
 });
