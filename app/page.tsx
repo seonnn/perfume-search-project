621e6c81de9567edebef85cac02331dc30c44a26
seonnn <ssni0105@gmail.com>
2024-03-06 12:35:32 +0900

    test: 메인페이지 SSR 적용

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { getNoteList } from '@/utils/supabase/getNoteList';
 import { getBrandList } from '@/utils/supabase/getBrandList';
 import FilterButton from '@/components/home/FilterButton';
 
-export const revalidate = 3600;
+export const dynamic = 'force-dynamic';
 
 async function Home({ searchParams }: { searchParams: { note: string; brand: string } }) {
   const perfumeList =
