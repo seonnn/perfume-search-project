ac8e1ee9a770a51b187d88415a49a7d8173638dc
seonnn <ssni0105@gmail.com>
2024-03-20 00:41:28 +0900

    fix: 빌드 에러 해결

--- app/api/fragranceNoteList/route.ts
+++ app/api/fragranceNoteList/route.ts
@@ -2,36 +2,36 @@ import { FragranceNoteListResponseData } from '@/types/response';
 import { supabase } from '@/utils/supabase/supabase';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  try {
-    const { data } = await supabase
-      .from('fragrance_list')
-      .select(
-        `
-      f_id,
-      note_list (
-        n_id,
-        n_name
-      )
-    `
-      )
-      .returns<FragranceNoteListResponseData[]>();
+// export async function GET() {
+//   try {
+//     const { data } = await supabase
+//       .from('fragrance_list')
+//       .select(
+//         `
+//       f_id,
+//       note_list (
+//         n_id,
+//         n_name
+//       )
+//     `
+//       )
+//       .returns<FragranceNoteListResponseData[]>();
 
-    if (!data || !data.length) {
-      return NextResponse.json([]);
-    }
+//     if (!data || !data.length) {
+//       return NextResponse.json([]);
+//     }
 
-    return NextResponse.json(
-      data.map((note) => {
-        const noteList = note.note_list.map((item) => {
-          return { id: item.n_id, name: item.n_name };
-        });
+//     return NextResponse.json(
+//       data.map((note) => {
+//         const noteList = note.note_list.map((item) => {
+//           return { id: item.n_id, name: item.n_name };
+//         });
 
-        return { fragranceId: note.f_id, noteList };
-      })
-    );
-  } catch (error) {
-    console.error(error);
-    throw new Error('노트 조회 실패');
-  }
-}
+//         return { fragranceId: note.f_id, noteList };
+//       })
+//     );
+//   } catch (error) {
+//     console.error(error);
+//     throw new Error('노트 조회 실패');
+//   }
+// }
